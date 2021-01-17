0fb726efc24c93c58f65552c7b6860d58258d824
Midhun <midun.kr@inapp.com>
2021-01-17 20:51:47 +0530

    second commit

---
+++ test.py
@@ -0,0 +1,9 @@
+class Man:
+    def __init__(self):
+        self.name="Man"
+        self.age=90
+    def say_hello(self):
+        print("hi i am "+self.name+" and i am",self.age," years old")
+man=Man()
+man.say_hello()
+    
