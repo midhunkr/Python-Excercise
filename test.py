27530b7e938f62420d3ca6f8ef069487b763c58f
Midhun <midun.kr@inapp.com>
2021-01-17 20:59:21 +0530

    Third commit

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-class Man:
+class Animal:
+    def __init__(self):
+        self.name="Animal"
+        self.alive=True
+    def isAlive(self):
+        if(self.alive):
+            print("This animal is alive")
+    
+class Man(Animal):
     def __init__(self):
         self.name="Man"
         self.age=90
     def say_hello(self):
         print("hi i am "+self.name+" and i am",self.age," years old")
+
 man=Man()
 man.say_hello()
-    
+anm=Animal()
+anm.isAlive()
